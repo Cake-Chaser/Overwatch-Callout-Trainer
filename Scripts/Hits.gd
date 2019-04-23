4bdc3b35f53dba3d7d5963a7588128a5eef7e246
CakeChaser <mattenuuk@gmail.com>
2019-04-23 13:14:46 +0200

    Fix the error Where when ther are multiple correct assers the 2'nd would break.
     Hanamura and dorado are now
    inheritance from parrent.tscn
    
    added af better font to Find_lbl

--- Scripts/Hits.gd
+++ Scripts/Hits.gd
@@ -38,27 +38,24 @@ func RandomeCallout():
 	random = randi() % All_btn.size()
 	The_Goal = All_btn[random].name
 	info_lbl.set_text("Find " + The_Goal )
+	for number in numbers:
+			if str(number) in The_Goal:
+				The_Goal = The_Goal.replace(number, "")
+				info_lbl.set_text("Find " + The_Goal )
 
 
-
-func _physics_process(delta):
+func _process(delta):
 	
 	
 	
 	
 	for btn in All_btn:
 		
-		for number in numbers:
-			if str(number) in The_Goal:
-				The_Goal = The_Goal.replace(number, "")
-				info_lbl.set_text("Find " + The_Goal )
-		
-		
 		if btn.pressed and The_Goal in str(btn.name) :
 			print(btn.name)
 			btn.visible = false
 			if All_btn.size() != 0:
-				All_btn.remove(random);
+				All_btn.remove(All_btn.find(btn));
 			RandomeCallout()
 	
 	
